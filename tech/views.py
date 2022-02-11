d0e0646930d5f777b11b7dc52473f4fa5ad65533
kelseypatterson817 <seymourintl@gmail.com>
2022-02-10 18:47:00 -0600

    Assignment 5 Resource Review Commit

--- tech/views.py
+++ tech/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import ProductType, Product, Review
 
 # Create your views here.
 def index (request):
     return render(request, 'techapp/index.html')
+
+def gettypes(request):
+    type_list=ProductType.objects.all()
+    return render(request, 'techapp/types.html' ,{'type_list' : type_list})
