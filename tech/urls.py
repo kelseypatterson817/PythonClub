25d3b236169f1edb097e16aa7058397ea015b4f4
kelseypatterson817 <seymourintl@gmail.com>
2022-03-01 17:21:07 -0600

    Assignment 6 Detail Commit

--- tech/urls.py
+++ tech/urls.py
@@ -6,3 +6,22 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('gettypes/', views.gettypes, name='types'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.index, name='index'),
+    path('gettypes/', views.gettypes, name='types'),
+    path('getproducts/', views.getproducts, name='products')
+]
+
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('', views.index, name='index'),
+    path('gettypes/', views.gettypes, name='types'),
+    path('getproducts/', views.getproducts, name='products'),
+    path('productdetails/<int:id>', views.productdetails, name='productdetails'),
+]
--- tech/views.py
+++ tech/views.py
@@ -8,3 +8,18 @@ def index (request):
 def gettypes(request):
     type_list=ProductType.objects.all()
     return render(request, 'techapp/types.html' ,{'type_list' : type_list})
+
+def getproducts(request):
+    products_list=Product.objects.all()
+    return render(request, 'techapp/products.html', {'products_list': products_list})
+    
+def productdetails(request, id):
+    prod=get_object_or_404(Product, pk=id)
+    discount=prod.memberdiscount
+    reviews=Review.objects.filter(product=id).count()
+    context={
+        'prod' : prod,
+        'discount' : discount,
+        'reviews' : reviews,
+    }
+    return render(request, 'techapp/proddetails.html', context=context)
